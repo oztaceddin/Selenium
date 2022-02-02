8dae6fc1b721244ee1a2fa06d76901f44361a89d
taceddin öz <taceddin95@gmail.com>
2022-02-02 21:20:17 +0300

    silinme

--- src/test/java/tests/deneme.java
+++
@@ -1,8 +0,0 @@
-package tests;
-
-public class deneme {
-
-    public static void main(String[] args) {
-        System.out.println("dfdsf");
-    }
-}
--- src/test/java/tests/deneme2.java
+++
@@ -1,7 +0,0 @@
-package tests;
-
-public class deneme2 {
-    public static void main(String[] args) {
-        System.out.println("dgdxf");
-    }
-}
